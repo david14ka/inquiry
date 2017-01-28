c95bedec0a5be7d8f665cb57b92bf0133e8615dd
Aidan Follestad <drummer.aidan@gmail.com>
2017-01-27 22:50:53 -0600

    Instrument tests should be complete now.

--- library/src/androidTest/java/com/afollestad/inquiry/Person.java
+++ library/src/androidTest/java/com/afollestad/inquiry/Person.java
@@ -14,15 +14,17 @@ import com.afollestad.inquiry.lazyloading.LazyLoaderList;
         // Default constructor is needed so Inquiry can auto construct instances
     }
 
-    public Person(String name) {
+    public Person(String name, int age) {
         this.id = 0;
         this.name = name;
+        this.age = age;
         this.children = new LazyLoaderList<>(0);
     }
 
     @Column(name = "_id", primaryKey = true, notNull = true, autoIncrement = true)
     public long id;
     @Column public String name;
+    @Column public int age;
 
     // inverseFieldName is optional, here it refers to the "parent" field in the Child class which gets set to a reference to this Person
     @ForeignKey(tableName = "children", foreignColumnName = "parentId", inverseFieldName = "parent")
